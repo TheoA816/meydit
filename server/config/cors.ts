8cb46eb403244b80e3512a4a20bbe84987710075
Theo Ang <theo.ang816@gmail.com>
2023-04-30 14:49:11 +1000

    add be to allowed cors

--- server/config/cors.ts
+++ server/config/cors.ts
@@ -44,7 +44,11 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin: ['https://meydit-fe.onrender.com/', 'https://meydit-fe.onrender.com', 'https://meydit-fe.netlify.app/', 'https://meydit-fe.netlify.app', 'http://localhost:5173'],
+  origin: ['https://meydit-fe.onrender.com/', 'https://meydit-fe.onrender.com',
+           'https://meydit-fe.netlify.app/', 'https://meydit-fe.netlify.app',
+           'http://localhost:5173/', 'http://localhost:5173',
+           'http://localhost:3333/', 'http://localhost:3333',
+           'https://meydit-be.onrender.com/', 'https://meydit-be.onrender.com'],
 
   /*
   |--------------------------------------------------------------------------
